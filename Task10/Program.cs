ea0bf53d76239788ccb8eada02c545b12cfbb2ae
Maksim <brazhmax@mail.ru>
2022-08-11 22:09:52 +0300

    add Task 10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,1 +1,20 @@
-﻿// Задача 10
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+using static System.Console;
+
+WriteLine("Введите целое трёхзначное число: ");
+int number = int.Parse(ReadLine());
+int [] array = new int[2];
+int i = 0;
+int razryad = 1;
+if (number >= 100 && number <= 999)
+{
+while (i < 2)
+{
+array [i] = (number / razryad) % razryad;
+razryad = razryad * 10;
+i++;
+}
+WriteLine($"Вторая цифра числа: {array[1]}");
+}
+else
+WriteLine("Число не трехзначное!");
--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,1 +1,39 @@
-﻿// Задача 13
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+using static System.Console;
+
+int digitCount(int number)
+{
+    int i = 0;
+    while (number != 0)
+    {
+        number /= 10;  
+        i++;
+    }
+    return i;
+}
+
+int MyPow(int a, int n)
+{
+int myresult = 1;
+for (int i = 0; i<n; i++)
+myresult *= a;
+return myresult;
+}
+
+WriteLine("Введите целое число: ");
+int number = int.Parse(ReadLine());
+int [] array = new int [3];
+int i = 0;
+int razryad = digitCount(number);
+if (number >= 100)
+{
+while (i < 3)
+{
+array [i] = (number %  MyPow(10, razryad) ) / MyPow(10, razryad-1);
+razryad--;
+i++;
+}
+WriteLine($"Третья цифра числа: {array[2]}");
+}
+else
+WriteLine("Число не трехзначное, третьей цифры нет!");
